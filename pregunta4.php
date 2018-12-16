3ffc6078b5629c20ea39436cde67350a65f43ffe
LAB-USR-AQ265-A0806 <LAB-USR-AQ265-A0806@grupoutp.edu.pe>
2018-12-15 20:32:02 -0500

    final

--- pregunta4.php
+++ pregunta4.php
@@ -20,17 +20,26 @@
         $con = mysqli_connect('localhost','root','','mibase') or die('Error al conectar al servidor MySQL.');
         $usuario = $_POST['usuario'];
         $password = $_POST['password'];
-
         $cadCon = "SELECT * FROM usuario WHERE usuario = '{$usuario}' AND password = '{$password}' ";
         mysqli_query($con, $cadCon) or die('Error al buscar en la base de datos.');
         $setFil = mysqli_query($con, $cadCon);
         $fil = mysqli_fetch_array($setFil);
         if (empty($fil)) {
-            #INSTRUCCION SQL INSERT
-            $insSql="INSERT INTO usuario (usuario, password) VALUES('{$usuario}','{$password}')";
-            mysqli_query($con,$insSql);
-            mysqli_close($con);
-            echo "Se guardo con exito";
+            $cadCon = "SELECT count(*) FROM usuario";
+            mysqli_query($con, $cadCon) or die('Error al buscar en la base de datos.');
+            $setFil = mysqli_query($con, $cadCon);
+            $fil2 = mysqli_fetch_array($setFil);
+            if(!empty($fil2[0]) && $fil2[0]<5){
+                #INSTRUCCION SQL INSERT
+                 $insSql="INSERT INTO usuario (usuario, password) VALUES('{$usuario}','{$password}')";
+                    mysqli_query($con,$insSql);
+                     mysqli_close($con);
+                 echo "Se guardo con exito";
+            } else {
+                             echo "ya existen 5 usuarios";
+
+        }
+               
         } else {
             echo "el usuario existe";
         }
